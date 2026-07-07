494ed4933377441dd65f108165c43cfb86d946c0
Artem Golubtsov <artgolubtsov@gmail.com>
2026-07-07 10:59:38 +0300

    feat: simplify brands layout, sidebar moved to Shell

--- app/brands/[brand]/layout.tsx
+++ app/brands/[brand]/layout.tsx
@@ -1,7 +1,5 @@
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
-import { Sidebar } from "@/components/Sidebar"
-import { RoleSwitcher } from "@/components/RoleSwitcher"
 import { BRAND_SLUGS } from "@/lib/brands"
 
 export default async function BrandLayout({
@@ -15,18 +13,7 @@ export default async function BrandLayout({
   if (!session) redirect("/login")
 
   const { brand } = await params
+  if (!BRAND_SLUGS.includes(brand as never)) redirect("/brands")
 
-  if (!BRAND_SLUGS.includes(brand as never)) {
-    redirect("/")
-  }
-
-  return (
-    <div className="flex min-h-screen">
-      <Sidebar brandSlug={brand} userName={session.user?.name} />
-      <main className="flex-1 min-w-0 bg-[#fafafa]">
-        {children}
-      </main>
-      <RoleSwitcher />
-    </div>
-  )
+  return <>{children}</>
 }
